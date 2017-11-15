b669d5c29a55c2cd2bfddef68643145fd2c3aeb8
Rostradamus <hyungro@hotmail.com>
2017-11-14 17:24:15 -0800

    merged

--- src/db/DBconfig.ts
+++ src/db/DBconfig.ts
@@ -2,7 +2,7 @@ export default class DBconfig {
     private static db_info: any = {
         host: "localhost",
         user: "root",
-        password: "wjswlals",
+        password: "Dlgudfh1@",
         database: "FlightManager",
         dateStrings: 'date'
     };
