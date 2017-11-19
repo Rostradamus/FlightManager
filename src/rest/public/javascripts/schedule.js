928737cf7bd95b03df27c5d2fcfe1f0b8ceb1e7b
Rostradamus <hyungro@hotmail.com>
2017-11-19 00:56:44 -0800

    quickFix

--- src/rest/public/javascripts/schedule.js
+++ src/rest/public/javascripts/schedule.js
@@ -222,7 +222,7 @@ $(document).ready(function () {
 
     $(document).on("click", "#clear-all-schedule", function(){
         clearResult_all();
-    })
+    });
 
     $(document).on("click", "#logout", function () {
         session.clear();
